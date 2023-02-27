f8626b52c630de08e1e70e9c7d6a20e66d0c3557
Vadim Smirnov <110857046+perch97@users.noreply.github.com>
2023-02-27 14:45:16 -0800

    Create reading_from_a_text_file_2.cpp

---
+++ reading_from_a_text_file/reading_from_a_text_file_2.cpp
@@ -0,0 +1,19 @@
+//Read and display Shakespeare Sonnet 18 poem using getline
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+int main(){
+    std::ifstream in_file;
+    in_file.open("poem.txt");
+    if(!in_file){
+        std::cerr<<"Problem opening file"<<std::endl;
+        return 1;//std::exit(1);
+    }
+    std::string line{};
+    while(std::getline(in_file,line)){ //  !in_file.eof()
+        std::cout<<line<<std::endl;
+    }
+    
+    std::cout<<"File is good to go"<<std::endl;
+    in_file.close();
+}
